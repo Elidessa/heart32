13a75e6b02ad06cbd526200483ac831bf59fbd2d
minipops <maximilian.knut@gmail.com>
2026-03-12 14:30:05 +0100

    lite fix av scale y som kördes för varje pixel varje frame lite onödigt

--- main/main.c
+++ main/main.c
@@ -1,6 +1,5 @@
 #include "esp_lcd_panel_ops.h"
 #include "esp_lcd_types.h"
-#include "esp_timer.h"
 #include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
 #include <stdint.h>
@@ -10,74 +9,63 @@
 #include "font.h"
 #include "lcd.h"
 #include "sampler.h"
-#include <esp_task_wdt.h>
 
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 32
+#define CANVAS_SIZE (DISPLAY_WIDTH * DISPLAY_HEIGHT / 8)
 
-static int plot_buf[128] = {0};
+static int plot_buf[DISPLAY_WIDTH] = {0};
 
 void update_waveform(int val);
 
-
-int scale_y(int val) {
-  static int min = 4095, max = 1;
-  int current_max = 1,current_min = 4095;
-
-	for (int i = 0; i < 128; i++) {
+static void get_waveform_range(int *out_min, int *out_range) {
+  int min = plot_buf[0], max = plot_buf[0];
+  for (int i = 1; i < DISPLAY_WIDTH; i++) {
     if (plot_buf[i] < min)
       min = plot_buf[i];
-			current_min = plot_buf[i];
     if (plot_buf[i] > max)
       max = plot_buf[i];
-			current_max = plot_buf[i];
   }
+  *out_min = min;
+  *out_range = (max - min > 10) ? (max - min) : 10;
+}
 
-	for (int i = 0; i < 128; i++) {
-    if (plot_buf[i] < current_min)
-			current_min = plot_buf[i];
-    if (plot_buf[i] > current_max)
-			current_max = plot_buf[i];
-  }
-
-	if(((float)current_max/max) < 2 && current_max > 300){
-			min = current_min;
-			max = current_max;
-	}
-		
-  int range = (max - min > 10) ? (max - min) : 10;
-  return 31 - ((val - min) * 31 / range);
+static int scale_y(int val, int min, int range) {
+  return (DISPLAY_HEIGHT - 1) - ((val - min) * (DISPLAY_HEIGHT - 1) / range);
 }
 
 void display_task(void *pvParameters) {
   esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t)pvParameters;
-  uint8_t *canvas = malloc(128 * 32 / 8);
-  memset(canvas, 0, 512);
+  uint8_t *canvas = malloc(CANVAS_SIZE);
   int sample;
   while (1) {
     while (xQueueReceive(sample_queue, &sample, 0) == pdTRUE) {
       update_waveform(sample);
     }
 
-    memset(canvas, 0, 512);
+    memset(canvas, 0, CANVAS_SIZE);
 
-    for (int x = 0; x < 127; x++) {
-      int y = scale_y(plot_buf[x]);
-      int next_y = scale_y(plot_buf[x + 1]);
+    int min, range;
+    get_waveform_range(&min, &range);
+
+    for (int x = 0; x < DISPLAY_WIDTH-1; x++) {
+      int y = scale_y(plot_buf[x], min, range);
+      int next_y = scale_y(plot_buf[x + 1], min, range);
       draw_line(x, y, x + 1, next_y, canvas);
     }
-		//text på skärmen
-		char text[20];
-		sprintf(text, "BPM: %d",bpm);
-		draw_text(text, 1, 0, canvas);
-//		animate_heart(panel_handle, canvas);
-		esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, 128, 32, canvas);
-		//
-		vTaskDelay(pdMS_TO_TICKS(50));
+    // text på skärmen
+    char text[20];
+    snprintf(text, sizeof(text), "BPM: %d", bpm);
+    draw_text(text, 1, 0, canvas);
+    esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT,
+                              canvas);
+    vTaskDelay(pdMS_TO_TICKS(50));
   }
 }
 
 void update_waveform(int val) {
-  memmove(plot_buf, plot_buf + 1, sizeof(int) * (128 - 1));
-	plot_buf[127] = val;
+  memmove(plot_buf, plot_buf + 1, sizeof(int) * (DISPLAY_WIDTH - 1));
+  plot_buf[DISPLAY_WIDTH - 1] = val;
 }
 
 void app_main(void) {
